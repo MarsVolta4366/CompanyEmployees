1c223305ab708aa4b54f7c8db61b3038037f927a
unknown <derek.slauson5@gmail.com>
2023-03-12 21:33:07 -0500

    Setup CompaniesController to utilize repository pattern.

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -11,5 +11,10 @@ namespace Repository
         {
 
         }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+            FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+            .ToList();
     }
 }
